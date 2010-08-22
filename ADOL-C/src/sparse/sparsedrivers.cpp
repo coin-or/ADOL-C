6ed518d462008f5d9bace252eea120879b0744b2
Kshitij Kulshreshtha <kkhere.geo@gmail.com>
2010-08-22 22:12:17 +0200

    avoid invalid memory write and segfault

--- ADOL-C/src/sparse/sparsedrivers.cpp
+++ ADOL-C/src/sparse/sparsedrivers.cpp
@@ -343,11 +343,13 @@ int sparse_jac(
 	{
 	  g->GenerateSeedJacobian(&(sJinfos.Seed), &(sJinfos.seed_rows), 
 				  &(sJinfos.seed_clms), "SMALLEST_LAST","ROW_PARTIAL_DISTANCE_TWO"); 
+	  sJinfos.seed_clms = indep;
 	}  
       else
 	{
 	  g->GenerateSeedJacobian(&(sJinfos.Seed), &(sJinfos.seed_rows), 
                                 &(sJinfos.seed_clms), "SMALLEST_LAST","COLUMN_PARTIAL_DISTANCE_TWO"); 
+	  sJinfos.seed_rows = depen;
 	}
       
       sJinfos.B = myalloc2(sJinfos.seed_rows,sJinfos.seed_clms);
