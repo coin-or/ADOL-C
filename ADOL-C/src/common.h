ccbcad1deae350bbddc61618e4e4d3d2442f2a67
Kshitij Kulshreshtha <kkhere.geo@gmail.com>
2010-08-31 14:33:34 +0000

    define separate MIN/MAXDEC macros for real and int
    MINDEC is used mostly for int so MINDECR for real
    MAXDEC is used mostly for real so MAXDECI for int.
    
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.hu-berlin.de>

--- ADOL-C/src/common.h
+++ ADOL-C/src/common.h
@@ -97,8 +97,10 @@
 #  define END_C_DECLS
 #endif
 
-#define MAXDEC(a,b) do { register typeof(a) __r; if ( __r = (b) > (a) ) (a) = __r; } while (0)
-#define MINDEC(a,b) do { register typeof(a) __r; if ( __r = (b) < (a) ) (a) = __r; } while (0)
+#define MAXDEC(a,b) do { register revreal __r = (b); if ( __r > (a) ) (a) = __r; } while (0)
+#define MAXDECI(a,b) do { register int __r = (b); if ( __r > (a) ) (a) = __r; } while (0)
+#define MINDECR(a,b) do { register revreal __r = (b); if ( __r < (a) ) (a) = __r; } while (0)
+#define MINDEC(a,b) do { register int __r = (b); if ( __r < (a) ) (a) = __r; } while (0)
 
 #define MAX_ADOLC(a,b) ( (a)<(b)? (b):(a) )
 #define MIN_ADOLC(a,b) ( (a)>(b)? (b):(a) )
