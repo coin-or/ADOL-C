3124e9f7870cb0b26612a291e46e4f7adcce5ffa
Andrea Walther <andrea.walther@math.hu-berlin.de>
2011-06-26 19:05:17 +0000

    new files nlf_forward_*.c for propagation of nonlinear frontiers

---
+++ ADOL-C/src/nlf_forward_s.c
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------
+ ADOL-C -- Automatic Differentiation by Overloading in C++
+ File:     int_forward_s.c
+ Revision: $Id: indopro_forward_s.c 42 2009-07-15 18:37:17Z awalther $
+ Contents: int_forward (integer forward mode for bit pattern propagation)
+ 
+ Copyright (c) Andrea Walther
+  
+ This file is part of ADOL-C. This software is provided as open source.
+ Any use, reproduction, or distribution of the software constitutes 
+ recipient's acceptance of the terms of the accompanying license file.
+ 
+----------------------------------------------------------------------------*/
+#define _INDO_ 1
+#define _NLF_ 1
+#define _NTIGHT_ 1
+#include  <uni5_for.c>
+#undef _INDO_
+#undef _NLF_
+#undef _NTIGHT_
+
