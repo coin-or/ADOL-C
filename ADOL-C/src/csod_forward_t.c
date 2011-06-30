72a1f340d94b2f93eac1d512b57f0f8524abf61c
Andrea Walther <andrea.walther@math.hu-berlin.de>
2011-06-30 13:23:02 +0000

    corresponding csod_forward_* files

---
+++ ADOL-C/src/csod_forward_t.c
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------
+ ADOL-C -- Automatic Differentiation by Overloading in C++
+ File:     csod_forward_t.c
+
+ Copyright (c) Andrea Walther
+  
+ This file is part of ADOL-C. This software is provided as open source.
+ Any use, reproduction, or distribution of the software constitutes 
+ recipient's acceptance of the terms of the accompanying license file.
+ 
+----------------------------------------------------------------------------*/
+#define _INDO_ 1
+#define _CSOD_ 1
+#define _TIGHT_ 1
+#include  <uni5_for.c>
+#undef _INDO_
+#undef _CSOD_
+#undef _TIGHT_
+
