4d91c6660ab65a3c070f761cbf09b1077824d059
Andrea Walther <andrea.walther@math.hu-berlin.de>
2012-08-28 09:45:09 +0000

    set derivative values array pointer to NULL in MittelmannDistCntrlNeumA_sparse

--- ADOL-C/examples/additional_examples/ipopt/MittelmannDistCntrlNeumA_sparse/ADOL-C_sparseNLP.cpp
+++ ADOL-C/examples/additional_examples/ipopt/MittelmannDistCntrlNeumA_sparse/ADOL-C_sparseNLP.cpp
@@ -432,7 +432,8 @@ void MyADOLC_sparseNLP::generate_tapes(Index n, Index m, Index& nnz_jac_g, Index
   options_g[2] = -1;         /*                     &jacval is not computed */ 
   options_g[3] = 0;          /*                column compression (default) */ 
   
-  jacval=NULL;
+  this->jacval=NULL;
+  this->hessval=NULL;
   sparse_jac(tag_g, m, n, 0, xp, &nnz_jac, &rind_g, &cind_g, &jacval, options_g); 
 
   options_g[2] = 0;
