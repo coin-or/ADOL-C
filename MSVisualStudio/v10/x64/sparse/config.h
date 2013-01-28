c573fd4141bf6d22db8354c209d4c2112d19241c
Kshitij Kulshretha <kkhere.geo@gmail.com>
2013-01-28 15:19:47 +0000

    Fix typo in config file
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.upb.de>

--- MSVisualStudio/v10/x64/sparse/config.h
+++ MSVisualStudio/v10/x64/sparse/config.h
@@ -100,7 +100,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the `trunc' function. */
-/ #undef HAVE_TRUNC */
+/* #undef HAVE_TRUNC */
 
 /* Define to 1 if you have the <unistd.h> header file. */
 /* #undef HAVE_UNISTD_H */
