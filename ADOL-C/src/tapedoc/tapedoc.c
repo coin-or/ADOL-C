62b98c2269fe562f9821f17cb9df704aa48b1ac5
Jean Utke <utke@mcs.anl.gov>
2013-09-06 08:56:11 -0500

    various fixes to the tapedoc so the ouput makes actual sense

--- ADOL-C/src/tapedoc/tapedoc.c
+++ ADOL-C/src/tapedoc/tapedoc.c
@@ -159,7 +159,7 @@ void filewrite( unsigned short opcode, const char* opString, int nloc, int *loc,
             fprintf(fp," &");
         for(i=0; i<ncst-1; i++)
             fprintf(fp,"$ %e $ &",cst[i]);
-        fprintf(fp,"$ %e $",val[ncst-1]);
+        fprintf(fp,"$ %e $",cst[ncst-1]);
     } else {
         fprintf(fp," &");
         fprintf(fp," ");
@@ -247,7 +247,7 @@ void tape_doc(short tnum,         /* tape id */
 
     /* interface temporaries */
     int loc_a[maxLocsPerOp];
-    double val_a[4], cst_d[2];
+    double val_a[4]={0,0,0,0}, cst_d[2]={0,0};
     const char* opName;
 
     ADOLC_OPENMP_THREAD_NUMBER;
@@ -1310,33 +1310,37 @@ void tape_doc(short tnum,         /* tape id */
 	      break;
 
 	    case ampi_gather:
-	      TAPE_AMPI_read_int(loc_a+1); /* commSizeForRootOrNull */
-	      if (*(loc_a+1)>0) {
-	        TAPE_AMPI_read_int(loc_a+2); /* rcnt */
-	        loc_a[2] = get_locint_f(); /* rbuf loc */
+	      size=0;
+	      TAPE_AMPI_read_int(loc_a+size++); /* commSizeForRootOrNull */
+	      if (*(loc_a+0)>0) {
+	        loc_a[size++] = get_locint_f(); /* rbuf loc */
+	        TAPE_AMPI_read_int(loc_a+size++); /* rcnt */
 	        TAPE_AMPI_read_MPI_Datatype(&anMPI_Datatype); /* rtype */
 	      }
-	      TAPE_AMPI_read_int(loc_a+2); /* count */
+	      loc_a[size++]=get_locint_f(); /* buf loc */
+	      TAPE_AMPI_read_int(loc_a+size++); /* count */
 	      TAPE_AMPI_read_MPI_Datatype(&anMPI_Datatype); /* type */
-	      TAPE_AMPI_read_int(loc_a+4); /* root */
+	      TAPE_AMPI_read_int(loc_a+size++); /* root */
 	      TAPE_AMPI_read_MPI_Comm(&anMPI_Comm);
-	      TAPE_AMPI_read_int(loc_a+1); /* commSizeForRootOrNull */
-	      filewrite_ampi(operation, "ampi gather",5, loc_a);
+	      TAPE_AMPI_read_int(loc_a+0); /* commSizeForRootOrNull */
+	      filewrite_ampi(operation, "ampi gather",size, loc_a);
 	      break;
 
 	    case ampi_scatter:
-	      TAPE_AMPI_read_int(loc_a+1); /* commSizeForRootOrNull */
-              if (*(loc_a+1)>0) {
-	        TAPE_AMPI_read_int(loc_a+2); /* rcnt */
-	        loc_a[2] = get_locint_f(); /* rbuf loc */
+	      size=0;
+	      TAPE_AMPI_read_int(loc_a+size++); /* commSizeForRootOrNull */
+	      if (*(loc_a+0)>0) {
+	        loc_a[size++] = get_locint_f(); /* rbuf loc */
+	        TAPE_AMPI_read_int(loc_a+size++); /* rcnt */
 	        TAPE_AMPI_read_MPI_Datatype(&anMPI_Datatype); /* rtype */
 	      }
-	      TAPE_AMPI_read_int(loc_a+2); /* count */
+	      loc_a[size++]=get_locint_f(); /* buf loc */
+	      TAPE_AMPI_read_int(loc_a+size++); /* count */
 	      TAPE_AMPI_read_MPI_Datatype(&anMPI_Datatype); /* type */
-	      TAPE_AMPI_read_int(loc_a+4); /* root */
+	      TAPE_AMPI_read_int(loc_a+size++); /* root */
 	      TAPE_AMPI_read_MPI_Comm(&anMPI_Comm);
-	      TAPE_AMPI_read_int(loc_a+1); /* commSizeForRootOrNull */
-	      filewrite_ampi(operation, "ampi scatter",5, loc_a);
+	      TAPE_AMPI_read_int(loc_a+0); /* commSizeForRootOrNull */
+	      filewrite_ampi(operation, "ampi scatter",size, loc_a);
 	      break;
 
 	    case ampi_allgather:
