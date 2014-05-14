fca0c22e911ab692e296a15a31d524485c08131e
Kshitij Kulshretha <kkhere.geo@gmail.com>
2014-05-14 13:00:51 +0000

    get rid of some more clang compiler warnings
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.upb.de>

--- ADOL-C/include/adolc/adtl.h
+++ ADOL-C/include/adolc/adtl.h
@@ -293,7 +293,7 @@ inline bool adouble::_do_indo() {
 
 inline void setNumDir(const size_t p) {
     if (refcounter::refcnt > 0) {
-	fprintf(DIAG_OUT, "ADOL-C Warning: Tapeless: Setting numDir will not change the number of\n directional derivative in existing adoubles and may lead to erronious results\n or memory corruption\n Number of currently existing adoubles = %z\n", refcounter::refcnt);
+	fprintf(DIAG_OUT, "ADOL-C Warning: Tapeless: Setting numDir will not change the number of\n directional derivative in existing adoubles and may lead to erronious results\n or memory corruption\n Number of currently existing adoubles = %zu\n", refcounter::refcnt);
     }
     if (p < 1) {
 	fprintf(DIAG_OUT, "ADOL-C Error: Tapeless: You are being a moron now.\n");
@@ -304,7 +304,7 @@ inline void setNumDir(const size_t p) {
 
 inline void setMode(enum Mode newmode) {
     if (refcounter::refcnt > 0) {
-	fprintf(DIAG_OUT, "ADOL-C Warning: Tapeless: Setting mode will the change the mode of\n computation in previously computed variables and may lead to erronious results\n or memory corruption\n Number of currently exisiting adoubles = %z\n", refcounter::refcnt);
+	fprintf(DIAG_OUT, "ADOL-C Warning: Tapeless: Setting mode will the change the mode of\n computation in previously computed variables and may lead to erronious results\n or memory corruption\n Number of currently exisiting adoubles = %zu\n", refcounter::refcnt);
     }
     adouble::forward_mode = newmode;
 }
