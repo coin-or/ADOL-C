9f6b1a30b512966899bfc19c38a16cace8f531ab
Andrea Walther <awalther@math.uni-paderborn.de>
2015-09-12 17:08:52 +0200

    psdriver first version

---
+++ ADOL-C/include/adolc/drivers/psdrivers.h
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------
+ ADOL-C -- Automatic Differentiation by Overloading in C++
+ File:     drivers/psdrivers.h
+ Revision: $Id$
+ Contents: Easy to use drivers for piecewise smooth functions
+           (with C and C++ callable interfaces including Fortran 
+           callable versions).
+
+ Copyright (c) Andrea Walther, Sabrina Fiege
+
+ This file is part of ADOL-C. This software is provided as open source.
+ Any use, reproduction, or distribution of the software constitutes 
+ recipient's acceptance of the terms of the accompanying license file.
+ 
+----------------------------------------------------------------------------*/
+#if !defined(ADOLC_DRIVERS_PSDRIVERS_H)
+#define ADOLC_DRIVERS_PSDRIVERS_H 1
+
+#include <adolc/internal/common.h>
+#include <adolc/interfaces.h>
+
+
+BEGIN_C_DECLS
+
+/****************************************************************************/
+/*                                                 DRIVERS FOR PS FUNCTIONS */
+
+/*--------------------------------------------------------------------------*/
+/*                                                               abs_normal */
+/*                              */
+ADOLC_DLL_EXPORT int abs_normal(short,int,int,double*,short*,double*,double*,double*,double*,double**,double**,double**,double**);
+ADOLC_DLL_EXPORT fint abs_normal_(fint,fint,fint,fdouble*,fint*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble**,fdouble**,fdouble**,fdouble**);
+
+
+END_C_DECLS
+
+/****************************************************************************/
+/****************************************************************************/
+/*                                                       Now the C++ THINGS */
+#if defined(__cplusplus)
+
+/****************************************************************************/
+/*                                                 DRIVERS FOR PS FUNCTIONS */
+
+/*--------------------------------------------------------------------------*/
+/*                                                          abs-normal form */
+
+int abs_normal(short tag,      /* tape identifier */ 
+               int m,          /* number od dependents   */             
+               int n,          /* number of independents */
+               double *x,      /* base point */ 
+               short *sigma_x, /* sigma of x */
+               double *y,      /* function value */
+               double *z,      /* switching variables */
+               double *cz,     /* first constant */
+               double *cy,     /* second constant */
+               double **a,      
+               double **b,
+               double **Z, 
+               double **L);
+
+#endif
+
+/****************************************************************************/
+
+#endif
+
---
+++ ADOL-C/src/drivers/psdrivers.c
@@ -0,0 +1,83 @@
+/*----------------------------------------------------------------------------
+ ADOL-C -- Automatic Differentiation by Overloading in C++
+ File:     drivers/psdrivers.c
+ Revision: $Id$
+ Contents: Easy to use drivers for piecewise smooth functions
+           (with C and C++ callable interfaces including Fortran 
+            callable versions).
+
+ Copyright (c) Andrea Walther, Sabrina Fiege 
+
+ This file is part of ADOL-C. This software is provided as open source.
+ Any use, reproduct ion, or distribution of the software constitutes 
+ recipient's acceptance of the terms of the accompanying license file.
+  
+----------------------------------------------------------------------------*/
+#include <adolc/drivers/psdrivers.h>
+#include <adolc/interfaces.h>
+#include <adolc/adalloc.h>
+#include "taping_p.h"
+
+#include <math.h>
+
+BEGIN_C_DECLS
+
+/****************************************************************************/
+/*                                                 DRIVERS FOR PS FUNCTIONS */
+
+/*--------------------------------------------------------------------------*/
+/*                                                          abs-normal form */
+
+int abs_normal(short tag,      /* tape identifier */ 
+               int m,          /* number od dependents   */             
+               int n,          /* number of independents */
+               double *x,      /* base point */ 
+               short *sigma,   /* sigma of x */
+               double *y,      /* function value */
+               double *z,      /* switching variables */
+               double *cz,     /* first constant */
+               double *cy,     /* second constant */
+               double **J,      
+               double **Y,
+               double **Z, 
+               double **L)
+{
+
+  int i,j,s;
+  double **res, tmp;
+  s=get_num_switches(tag);
+
+  res=(double**)myalloc2(m+s,n+s);
+
+  zos_pl_forward(tag,m,n,1,x,y,z);
+  for(i=0;i<m+s;i++){
+    fos_pl_reverse(tag,m,n,s,i,res[i]);
+  }
+
+  for(i=0;i<s;i++){
+    cz[i]=z[i];
+    for(j=0;j<n;j++){
+      Z[i][j]=res[i][j];
+    }
+    for(j=0;j<s;j++){
+      L[i][j]=res[i][j+n];	
+    }
+    for(j=0;j<i;j++){
+      cz[i] = cz[i]-L[i][j]*sigma[j]*z[j];	
+    }
+  }
+
+  for(i=0;i<m;i++){
+    cy[i]=y[i];
+    for(j=0;j<n;j++){
+      J[i][j]=res[i+s][j];	
+    }
+    for(j=0;j<s;j++){
+      Y[i][j]=res[i+s][j+n];	
+      cy[i] = cy[i]-Y[i][j]*sigma[j]*z[j];	
+    }
+  }
+}
+
+
+END_C_DECLS
