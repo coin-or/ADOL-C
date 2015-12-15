189fe8f5c3077c6c729731eea79ba8210d68f5cd
Kshitij Kulshreshtha <kkhere.geo@gmail.com>
2015-12-15 10:16:23 +0000

    few fixes
    - adubref must not be created by user or copied
    - adubref destructor was missing
    - we can have an explicit cast since we have a function
      to conver adub to adub*
    - erf() was not protected by ATRIG_ERF
    - fix function declarations in psdrivers.h
    
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.upb.de>

--- ADOL-C/include/adolc/drivers/psdrivers.h
+++ ADOL-C/include/adolc/drivers/psdrivers.h
@@ -3,15 +3,15 @@
  File:     drivers/psdrivers.h
  Revision: $Id$
  Contents: Easy to use drivers for piecewise smooth functions
-           (with C and C++ callable interfaces including Fortran
+           (with C and C++ callable interfaces including Fortran 
            callable versions).
 
  Copyright (c) Andrea Walther, Sabrina Fiege
 
  This file is part of ADOL-C. This software is provided as open source.
- Any use, reproduction, or distribution of the software constitutes
+ Any use, reproduction, or distribution of the software constitutes 
  recipient's acceptance of the terms of the accompanying license file.
-
+ 
 ----------------------------------------------------------------------------*/
 #if !defined(ADOLC_DRIVERS_PSDRIVERS_H)
 #define ADOLC_DRIVERS_PSDRIVERS_H 1
@@ -26,31 +26,14 @@ BEGIN_C_DECLS
 /*                                                 DRIVERS FOR PS FUNCTIONS */
 
 /*--------------------------------------------------------------------------*/
-/*                                              directional_active_gradient */
+/*                                             directional_active_gradient_ */
 /*                                                                          */
-ADOLC_DLL_EXPORT int directional_active_gradient(short,int,double*,short*,double*,double*,short*);
-ADOLC_DLL_EXPORT fint directional_active_gradient_(fint,fint,double*,short*,double*,double*,short*);
-/*--------------------------------------------------------------------------*/
-/*                                                               abs_normal */
-/*                                                                          */
-ADOLC_DLL_EXPORT int abs_normal(short,int,int,int,double*,short*,double*,double*,double*,double*,double**,double**,double**,double**);
-ADOLC_DLL_EXPORT fint abs_normal_(fint*,fint*,fint*,fint*,fdouble*,fint*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*);
-
-
-END_C_DECLS
-
-/****************************************************************************/
-/****************************************************************************/
-/*                                                       Now the C++ THINGS */
-#if defined(__cplusplus)
-
-/****************************************************************************/
-/*                                                 DRIVERS FOR PS FUNCTIONS */
-
+ADOLC_DLL_EXPORT fint directional_active_gradient_(fint,fint,double*,short*,double*,double*,short*);  
 /*--------------------------------------------------------------------------*/
 /*                                              directional_active_gradient */
 /*                                                                          */
-int directional_active_gradient(short tag,      /* trace identifier */
+ADOLC_DLL_EXPORT int directional_active_gradient(
+                                short tag,      /* trace identifier */
 				int n,          /* number of independents */
 				double* x,      /* value of independents */
 				short *sigma_x, /* sigma of x */
@@ -62,23 +45,26 @@ int directional_active_gradient(short tag,      /* trace identifier */
 /*--------------------------------------------------------------------------*/
 /*                                                               abs_normal */
 /*                                                                          */
-int abs_normal(short tag,      /* tape identifier */
-               int m,          /* number of dependents   */
-               int n,          /* number of independents */
-               double *x,      /* base point */
-               short *sigma_x, /* sigma of x */
-               double *y,      /* function value */
-               double *z,      /* switching variables */
-               double *cz,     /* first constant */
-               double *cy,     /* second constant */
-               double **a,
-               double **b,
-               double **Z,
-               double **L);
+ADOLC_DLL_EXPORT fint abs_normal_(fint*,fint*,fint*,fint*,fdouble*,fint*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*,fdouble*);
+ADOLC_DLL_EXPORT int abs_normal(short tag,      /* tape identifier */ 
+                                int m,          /* number od dependents   */ 
+                                int n,          /* number of independents */
+                                int swchk,      /* number of switches (check) */
+                                double *x,      /* base point */ 
+                                short *sigma,   /* sigma of x */
+                                double *y,      /* function value */
+                                double *z,      /* switching variables */
+                                double *cz,     /* first constant */
+                                double *cy,     /* second constant */
+                                double **J,      
+                                double **Y,
+                                double **Z, 
+                                double **L);
 
 
-#endif
+END_C_DECLS
 
 /****************************************************************************/
 
 #endif
+
