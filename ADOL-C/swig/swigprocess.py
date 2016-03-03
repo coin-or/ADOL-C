17c0d295d56b139314752bdb05fa782a1722ca12
Kshitij Kulshreshtha <kkhere.geo@gmail.com>
2016-03-03 17:07:43 +0100

    add copyright and licensing everywhere
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.upb.de>

--- ADOL-C/swig/swigprocess.py
+++ ADOL-C/swig/swigprocess.py
@@ -1,3 +1,15 @@
+##############################################################################
+## swigprocess.py -- Script to generate swig based interface for python and R
+## Revision: $Id$
+##
+## Copyright (C) Kshitij Kulshreshtha
+##
+## This file is part of ADOL-C. This software is provided as open source.
+## Any use, reproduction, or distribution of the software constitutes 
+## recipient's acceptance of the terms of the accompanying license file.
+## 
+##############################################################################
+
 import re
 import os.path
 import sys
