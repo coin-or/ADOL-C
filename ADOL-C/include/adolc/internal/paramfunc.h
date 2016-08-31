9961d1b9c5ade2d7cc1133f426c353808ca0c214
Kshitij Kulshretha <kkhere.geo@gmail.com>
2016-08-31 11:37:57 +0000

    add more inline wrappers for pdouble
    the proper function isn't resolved if getparam() or mkparam() is called
    within a unary function. The compiler tries to resolve it to the
    std::* function trying to convert pdouble to double,
    instead of the ADOL-C function by converting pdouble to adub.
    
    Signed-off-by: Kshitij Kulshreshtha <kshitij@math.upb.de>

--- ADOL-C/include/adolc/internal/paramfunc.h
+++ ADOL-C/include/adolc/internal/paramfunc.h
@@ -68,5 +68,50 @@
     inline friend adub fmax ( const badouble&, const pdouble& );
     inline friend adub fmin ( const pdouble&, const badouble& );
     inline friend adub fmin ( const badouble&, const pdouble& );
+    /*--------------------------------------------------------------------------*/
+    /* unary operators (friends) */
+    inline friend ADOLC_DLL_EXPORT adub exp  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub log  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub sqrt ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub sin  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub cos  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub tan  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub asin ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub acos ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub atan ( const pdouble& );
+
+    /*--------------------------------------------------------------------------*/
+    /* special operators (friends) */
+    /* no internal use of condassign: */
+    inline friend ADOLC_DLL_EXPORT adub    pow   ( const pdouble&, double );
+    inline friend ADOLC_DLL_EXPORT adub    log10 ( const pdouble& );
+
+    /* Additional ANSI C standard Math functions Added by DWJ on 8/6/90 */
+    inline friend ADOLC_DLL_EXPORT adub sinh  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub cosh  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub tanh  ( const pdouble& );
+#if defined(ATRIG_ERF)
+    inline friend ADOLC_DLL_EXPORT adub asinh ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub acosh ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub atanh ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub erf   ( const pdouble& );
+#endif
+
+    inline friend ADOLC_DLL_EXPORT adub fabs  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub ceil  ( const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub floor ( const pdouble& );
+
+    inline friend ADOLC_DLL_EXPORT adub fmax ( const pdouble&, const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub fmax ( double, const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub fmax ( const pdouble&, double );
+    inline friend ADOLC_DLL_EXPORT adub fmin ( const pdouble&, const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub fmin ( double, const pdouble& );
+    inline friend ADOLC_DLL_EXPORT adub fmin ( const pdouble&, double );
+
+    inline friend ADOLC_DLL_EXPORT adub ldexp ( const pdouble&, int );
+    inline friend ADOLC_DLL_EXPORT adub frexp ( const pdouble&, int* );
+
+    /*--------------------------------------------------------------------------*/
+
 #endif
 #endif
