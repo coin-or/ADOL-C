1165ad60b07e4a4527d6726913df02d635d56411
mflehmig <martin.flehmig@tu-dresden.de>
2018-07-02 14:31:50 +0200

    Implement parallel mat_jac and jac_mat method

--- ADOL-C/src/drivers/pardrivers.cpp
+++ ADOL-C/src/drivers/pardrivers.cpp
@@ -5,15 +5,68 @@
 #include <adolc/adalloc.h>
 #include "taping_p.h"
 
-/* Write thread local part of Jacobian to global Jacobian. */
-void writeLocJac2globJac(double** loc_jacobian, double** jacobian, int depen,
-                         int part, int loc_start)
+#include <iostream>
+
+static int get_thread_num()
 {
-  int j, k;
-  for (int i = 0; i < depen; ++i) {
-    for (k = 0, j = loc_start; j < loc_start+part; ++k, ++j)
-      jacobian[i][j] = loc_jacobian[i][k];
+#ifdef _OPENMP
+  return omp_get_thread_num();
+#else
+  return 0;
+#endif
+}
+
+static uint get_num_threads(int num)
+{
+  uint num_threads = 1;
+#ifdef _OPENMP
+  if (omp_get_max_threads() > num) {
+    num_threads = num;
+    omp_set_num_threads(num);
   }
+  else {
+    num_threads = omp_get_max_threads();
+  }
+#endif
+  return num_threads;
+}
+
+/**
+ * Returns the submatrix S containing nrows consecutive rows and ncols consecutive
+ * columns from given matrix U starting from row_st row and col_st column,
+ * respectively.
+ *
+ * U: (m, n)
+ * S: (nrows, ncols)
+ */
+static double** get_submat(const uint m, const uint n,
+                           const uint st_row, const uint nrows,
+                           const uint st_col, const uint ncols,
+                           double** U)
+{
+  // Do not touch rows and columns behind memory
+  assert((st_row+nrows) <= m);
+  assert((st_col+ncols) <= n);
+  double** loc_mat = myalloc2(nrows, ncols);
+  for (uint i = st_row, ii = 0; i < (st_row+nrows); ++i, ++ii)
+    for (uint j = st_col, jj = 0; j < (st_col+ncols); ++j, ++jj)
+      loc_mat[ii][jj] = U[i][j];
+
+  return loc_mat;
+}
+
+/* Verallgemeinerung: write block back, not just rows or columns */
+/*
+ * U : m, n
+ */
+static void writeLocMat2globMat(const int m, const int n,
+                                const uint st_row, const uint nrows,
+                                const uint st_col, const uint ncols,
+                                double** loc_jac, double** jacobian)
+{
+  for (uint i = 0, ii = st_row; ii < (st_row+nrows); ++i, ++ii)
+    for (uint j = 0, jj = st_col; jj < (st_col+ncols); ++j, ++jj)
+      jacobian[ii][jj] = loc_jac[i][j];
 }
 
 /* Jacobian Partitioning
@@ -26,16 +79,16 @@ void writeLocJac2globJac(double** loc_jacobian, double** jacobian, int depen,
  * If N%num_threads = r > 0, i.e., the first r threads are assigned an additional
  * row or column, respectively.
  */
-void calcPartitioning(int* part, int* loc_start, int num_threads, int N)
+void calcPartitioning(int* part, int* loc_start, uint num_threads, int N)
 {
   int tmppart = N / num_threads;
-  for (int i = 0; i < num_threads; ++i)
+  for (uint i = 0; i < num_threads; ++i)
     part[i] = tmppart;
-  for (int i = 0; i < N%num_threads; ++i)
+  for (uint i = 0; i < N%num_threads; ++i)
     ++part[i];
 
   loc_start[0] = 0;
-  for (int i = 1; i < num_threads; ++i)
+  for (uint i = 1; i < num_threads; ++i)
     loc_start[i] = loc_start[i-1] + part[i-1];
 }
 
@@ -49,18 +102,9 @@ int par_jacobian(short tag,
                  const double* argument,
                  double** jacobian)
 {
-  int num_threads = 1;
-#ifdef _OPENMP
-  if (omp_get_max_threads() > depen) {
-    num_threads = depen;
-    omp_set_num_threads(num_threads);
-  }
-  else {
-    num_threads = omp_get_max_threads();
-  }
-  fprintf(DIAG_OUT,"ADOL-C info: Parallel Jacobian method uses %i threads.\n",
+  uint num_threads = get_num_threads(depen);
+  fprintf(DIAG_OUT,"ADOL-C info: par_jacobian uses %i OpenMP-threads.\n",
           num_threads);
-#endif
 
   int rcg = 0;
   int* rc = new int[num_threads];
@@ -96,7 +140,7 @@ int par_jacobian(short tag,
       return rc[0];
     }
     else {
-      for (int i = 1; i < num_threads; ++i)
+      for (uint i = 1; i < num_threads; ++i)
         rc[i] = rc[0];
     }
     calcPartitioning(loc_part, loc_start, num_threads, depen);
@@ -106,7 +150,7 @@ int par_jacobian(short tag,
     //          num_threads, i, loc_part[i], i, loc_start[i]);
   }
 
-  // Dummy parallel region prevents undefined behaviour w.r.t. threadprivate + copyin.
+  // Dummy par. region prevents undef. behaviour w.r.t. threadprivate+copyin.
   // Leave it, even the compiler will encourage you to remove it!
   // Rationale: Reference threadprivate variable first, than copy value of
   //            master thread via copyin clause.
@@ -142,7 +186,7 @@ int par_jacobian(short tag,
     loc_jac = myalloc2(depen, loc_part[myid]);
     rc[myid] = fov_forward(tag,depen,indep, loc_part[myid], argument, loc_I, result, loc_jac);
     /* Each thread writes loc_jac into global Jacobian object. */
-    writeLocJac2globJac(loc_jac, jacobian, depen, loc_part[myid], loc_start[myid]);
+    writeLocMat2globMat(depen, indep, 0, depen, loc_start[myid], loc_part[myid], loc_jac, jacobian);
     /* free memory */
     myfreeI2nmp(indep, loc_part[myid], loc_start[myid], loc_I);
     myfree1(result);
@@ -159,7 +203,14 @@ int par_jacobian(short tag,
   myfree2(loc_jac);
 } /* parallel */
 
-  for (int i = 0; i < num_threads; ++i)
+#ifndef NDEBUG
+  std::cout << "ADOL-C info: Return codes of fov_reverse() within par_jac()"
+               " driver (Values < 0 indicate an error).\n";
+  for (uint i = 0; i < num_threads; ++i)
+    std::cout << "  Thread " << i << " : " << rc[i] << " \n";
+#endif
+
+  for (uint i = 0; i < num_threads; ++i)
     MINDEC(rcg, rc[i]);
 
   free(loc_start);
@@ -168,3 +219,176 @@ int par_jacobian(short tag,
 
   return rcg;
 }
+
+/* [in] m, n, p, U
+ * [out] Z
+ *
+ * Compute: Z = UJ(x_o)
+ * whereas J \in R^{m,n}, U in R^{p,m} and Z \in R^{p,n}
+ */
+int par_mat_jac(short tag, int m, int n, int p, const double* argument,
+                double** U, double** Z)
+{
+  uint num_threads = get_num_threads(p);
+  fprintf(DIAG_OUT,"ADOL-C info: par_jac_mat uses %i OpenMP-threads.\n",
+          num_threads);
+
+  int ret;
+  int* rc = new int[num_threads];
+  double* result = myalloc1(m);
+  ret = zos_forward(tag, m, n, 1, argument, result);
+  myfree1(result);
+  // Error? Clean up and return.
+  if (0 > ret) {
+    delete[] rc;
+    return ret;
+  }
+
+  // loc_part[i] := # rows assigned to thread i
+  int* loc_part = (int*)calloc(num_threads, sizeof(int));
+  if (NULL == loc_part) {
+    fail(ADOLC_MALLOC_FAILED);
+    adolc_exit(-1,"",__func__,__FILE__,__LINE__);
+  }
+  // loc_start[i] := row to start loc_part for thread i
+  int* loc_start = (int*)calloc(num_threads, sizeof(int));
+  if (NULL == loc_start) {
+    fail(ADOLC_MALLOC_FAILED);
+    adolc_exit(-1,"",__func__,__FILE__,__LINE__);
+  }
+  // Partitioning of rows of U
+  calcPartitioning(loc_part, loc_start, num_threads, p);
+
+#ifdef _OPENMP
+#pragma omp parallel default(none) shared(tag, rc, argument, U, Z, num_threads, loc_part, loc_start, p) \
+                                   copyin(ADOLC_OpenMP) \
+                                   firstprivate(m, n)
+#endif
+  {
+    int myid = get_thread_num();
+    double** loc_U;
+    // If we have only 1 thread, we don't want allocate additional memory.
+    if (1 < num_threads)
+      //loc_U = get_mat_part_rows(p, m, loc_start[myid], loc_part[myid], U);
+      loc_U = get_submat(p, m, loc_start[myid], loc_part[myid], 0, m, U);
+    else
+      loc_U = U;
+
+    // Since the result is a block of rows, we can directly let the loc_Z[i] point to the
+    // appropriate row in global Z
+    double** loc_Z = (double**)calloc(loc_part[myid], sizeof(double*));
+    for (int i = 0; i < loc_part[myid]; ++i)
+      loc_Z[i] = Z[loc_start[myid]+i];
+
+    rc[myid] = fov_reverse(tag, m, n, loc_part[myid], loc_U, loc_Z);
+
+    // Clean up.
+    myfree2(loc_Z);
+    if (1 < num_threads)
+      myfree2(loc_U);
+  } // end parallel
+
+  free(loc_start);
+  free(loc_part);
+
+#ifndef NDEBUG
+  std::cout << "ADOL-C info: Return codes of fov_forward() within par_jac_mat()"
+               " driver (Values < 0 indicate an error).\n";
+  for (size_t i = 0; i < num_threads; ++i)
+    std::cout << "  Thread " << i << " : " << rc[i] << " \n";
+#endif
+
+  int rcg = 0;
+  for (uint i = 0; i < num_threads; ++i)
+    MINDEC(rcg, rc[i]);
+
+  delete[] rc;
+
+  return rcg;
+}
+
+/* [in] tag, m, n, p, x, U
+ * [out] Z
+ *
+ * Compute: Z = J(x_o)U
+ * whereas J \in R^{m,n}, U in R^{n,p} and Z \in R^{m,p}
+ */
+int par_jac_mat(short tag, int m, int n, int p, const double* argument,
+                double** U, double** Z)
+{
+  uint num_threads = get_num_threads(p);
+  fprintf(DIAG_OUT,"ADOL-C info: par_jac_mat uses %i OpenMP-threads.\n",
+          num_threads);
+
+  int* rc = new int[num_threads];
+
+  // loc_part[i] := # columns assigned to thread i
+  int* loc_part = (int*)calloc(num_threads, sizeof(int));
+  if (NULL == loc_part) {
+    fail(ADOLC_MALLOC_FAILED);
+    adolc_exit(-1,"",__func__,__FILE__,__LINE__);
+  }
+  // loc_start[i] := column to start loc_part for thread i
+  int* loc_start = (int*)calloc(num_threads, sizeof(int));
+  if (NULL == loc_start) {
+    fail(ADOLC_MALLOC_FAILED);
+    adolc_exit(-1,"",__func__,__FILE__,__LINE__);
+  }
+  // Partitioning of columns of U
+  calcPartitioning(loc_part, loc_start, num_threads, p);
+
+  // Dummy par. region prevents undef. behaviour w.r.t. threadprivate+copyin.
+  // Leave it, even the compiler will encourage you to remove it!
+  // Rationale: Reference threadprivate variable first, than copy value of
+  //            master thread via copyin clause.
+#pragma omp parallel
+  {
+    ADOLC_OpenMP;
+  }
+
+#ifdef _OPENMP
+#pragma omp parallel default(none) shared(tag, rc, argument, U, Z, num_threads, loc_part, loc_start, p) \
+                                   copyin(ADOLC_OpenMP) \
+                                   firstprivate(m, n)
+#endif
+  {
+    int myid = get_thread_num();
+    double* result = myalloc1(m);
+    double** loc_U;
+    // If we have only 1 thread, we don't want we allocate additional memory.
+    if (1 < num_threads)
+      loc_U = get_submat(n, p, 0, n, loc_start[myid], loc_part[myid], U);
+    else
+      loc_U = U;
+
+    double** loc_Z = myalloc2(m, loc_part[myid]);
+    rc[myid] = fov_forward(tag, m, n, loc_part[myid], argument, loc_U, result, loc_Z);
+    /* Each thread writes loc_Z into global Z object. */
+    writeLocMat2globMat(m, p, 0, m, loc_start[myid], loc_part[myid], loc_Z, Z);
+
+    // Clean up.
+    myfree1(result);
+    myfree2(loc_Z);
+    if (1 < num_threads)
+      myfree2(loc_U);
+  } // end parallel
+
+  free(loc_start);
+  free(loc_part);
+
+#ifndef NDEBUG
+  std::cout << "ADOL-C info: Return codes of fov_forward() within par_jac_mat()"
+               " driver (Values < 0 indicate an error).\n";
+  for (size_t i = 0; i < num_threads; ++i)
+    std::cout << "  Thread " << i << " : " << rc[i] << " \n";
+#endif
+
+  int rcg = 0;
+  for (uint i = 0; i < num_threads; ++i)
+    MINDEC(rcg, rc[i]);
+
+  delete[] rc;
+
+  return rcg;
+}
+
