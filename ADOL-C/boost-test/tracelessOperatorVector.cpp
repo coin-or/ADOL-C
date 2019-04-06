7068ca05da167c224869fbc8d5d262a8e8c629a9
PhilippSchuette <p.schuette@online.de>
2019-04-06 23:21:07 +0200

    Add final Scalar and first Vector Tests.

--- ADOL-C/boost-test/tracelessOperatorVector.cpp
+++ ADOL-C/boost-test/tracelessOperatorVector.cpp
@@ -14,33 +14,36 @@ const size_t numDir = adtl::getNumDir();
 BOOST_AUTO_TEST_SUITE( traceless_vector )
 
 /***********************************/
-/* tests for traceless vector mode */
+/* Tests for traceless vector mode */
+/* Author: Philipp Schuette        */
 /***********************************/
 
-BOOST_AUTO_TEST_CASE(ExpOperatorDerivativeVectorMode) {
+BOOST_AUTO_TEST_CASE(ExpOperatorDerivativeVectorMode)
+{
   double a = 2.;
   adouble ad = a;
 
   for(size_t j = 0; j < numDir; j++) {
-    ad.setADValue(j,1.);
+    ad.setADValue(j, 1. + j*2.);
   }
 
   double aDerivative = std::exp(a);
   ad = adtl::exp(ad);
 
   for(size_t j = 0; j < numDir; j++) { 
-    BOOST_TEST(ad.getADValue(j) == aDerivative, tt::tolerance(tol));
+    BOOST_TEST(ad.getADValue(j) == aDerivative*(1. + j*2.), tt::tolerance(tol));
   }
 }
 
-BOOST_AUTO_TEST_CASE(MultOperatorDerivativeVectorMode) {
+BOOST_AUTO_TEST_CASE(MultOperatorDerivativeVectorMode)
+{
   double a = 2.5;
   double b = 4.;
   adouble ad = a;
   adouble bd = b;
 
-  ad.setADValue(0,1.);
-  bd.setADValue(1,1.);
+  ad.setADValue(0, 1.);
+  bd.setADValue(1, 1.);
 
   double aDerivative = b;
   double bDerivative = a;
@@ -51,4 +54,10 @@ BOOST_AUTO_TEST_CASE(MultOperatorDerivativeVectorMode) {
   BOOST_TEST(cd.getADValue(1) == bDerivative, tt::tolerance(tol));
 }
 
+
+
 BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
