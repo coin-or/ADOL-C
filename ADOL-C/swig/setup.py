ac89ea483bfdbe865ed940605cf12687073d07d0
Kshitij Kulshreshtha <kkhere.geo@gmail.com>
2020-09-26 00:17:51 +0200

    pass explicit libdir to setup.py and fix out-of-tree build even more
    libdir passed will only be used to find libadolc.so for linking,
    so that users who have explicitly set this in ./configure can still
    install.
    
    But python will still install its own module in:
    ${prefix}/lib64/python${py_version}/site-packages
    and ignore the given libdir for installation.
    
    This is the default python distutils behaviour and even if I could
    I will not change it.
    
    python distutils and autoconf/automake are not really compatible with
    each other.

--- ADOL-C/swig/setup.py
+++ ADOL-C/swig/setup.py
@@ -24,7 +24,7 @@ from distutils.command.build import build
 from distutils.command.install import install
 import subprocess
 
-def compile_dynlib(prefix,colpackdir,boostdir):
+def compile_dynlib(prefix,libdir,colpackdir,boostdir):
     import subprocess
     from multiprocessing import cpu_count
     nproc = cpu_count()
@@ -35,6 +35,7 @@ def compile_dynlib(prefix,colpackdir,boostdir):
         print(out)
         out = subprocess.check_output(['./configure',
                                        '--prefix=%s' % prefix,
+                                       '--libdir=%s' % libdir,
                                        '--with-colpack=%s' % colpackdir,
                                        '--with-boost=%s' % boostdir ],
                                       cwd=librootdir,
@@ -51,6 +52,7 @@ def compile_dynlib(prefix,colpackdir,boostdir):
 class build_adolc(Command):
     user_options = [
         ('lib-prefix=', None, 'prefix to install adolc library'),
+        ('lib-libdir=', None, 'libdir to install adolc library (usually inside prefix)'),
         ('colpack-dir=', None, 'directory in which colpack is installed'),
         ('boost-dir=', None, 'directory in which boost is installed'),
         ('only-swig', None, 'only install this swig module, not adolc library') ]
@@ -58,6 +60,7 @@ class build_adolc(Command):
 
     def initialize_options(self):
         self.lib_prefix = None
+        self.lib_libdir = None
         self.colpack_dir = None
         self.boost_dir = None
         self.only_swig = None
@@ -65,13 +68,14 @@ class build_adolc(Command):
     def finalize_options(self):
         self.set_undefined_options('build',
                                    ('lib_prefix','lib_prefix'),
+                                   ('lib_libdir','lib_libdir'),
                                    ('colpack_dir', 'colpack_dir'),
                                    ('boost_dir', 'boost_dir'),
                                    ('only_swig', 'only_swig') )
 
     def run(self):
         if not self.only_swig:
-            compile_dynlib(self.lib_prefix,self.colpack_dir,self.boost_dir)
+            compile_dynlib(self.lib_prefix,self.lib_libdir,self.colpack_dir,self.boost_dir)
         else:
             print('skipping build of ADOL-C')
 
@@ -79,26 +83,25 @@ class build_adolc(Command):
 class build_swigadolc(build_ext,object):
     command_name = 'build_ext'
     user_options = build_ext.user_options + [
-        ('lib-prefix=', None, 'prefix of installed adolc library') ]
+        ('lib-prefix=', None, 'prefix of installed adolc library'),
+        ('lib-libdir=', None, 'libdir to install adolc library (usually inside prefix)')
+    ]
     def initialize_options(self):
         super(build_swigadolc,self).initialize_options()
         self.lib_prefix = None
+        self.lib_libdir = None
 
     def finalize_options(self):
         super(build_swigadolc,self).finalize_options()
         self.set_undefined_options('build',
-                                   ('lib_prefix','lib_prefix'))
+                                   ('lib_prefix','lib_prefix'),
+                                   ('lib_libdir','lib_libdir'))
         prefix = self.lib_prefix
+        libdir = self.lib_libdir
         print('prefix = ', prefix)
         self.include_dirs.append(os.path.join(prefix,'include'))
-        import ctypes as c
-        plen = c.sizeof(c.c_void_p(0))
-        if plen == 8:
-            self.library_dirs.append(os.path.join(prefix,'lib64'))
-            self.rpath.append(os.path.join(prefix,'lib64'))
-        else:
-            self.library_dirs.append(os.path.join(prefix,'lib'))
-            self.rpath.append(os.path.join(prefix,'lib'))
+        self.library_dirs.append(libdir)
+        self.rpath.append(libdir)
         buildobj = self.get_finalized_command('build')
         self.src = buildobj.src
         prepare_flat_header(self.src)
@@ -120,6 +123,7 @@ class buildthis(build,object):
     user_options = build.user_options + [
         ('src=', None, 'path of the source directory of swig module'),
         ('lib-prefix=', None, 'prefix to install adolc library'),
+        ('lib-libdir=', None, 'libdir to install adolc library (usually inside prefix)'),
         ('colpack-dir=', None, 'directory in which colpack is installed'),
         ('boost-dir=', None, 'directory in which boost is installed'),
         ('only-swig', None, 'only install this swig module, not adolc library') ]
@@ -129,13 +133,14 @@ class buildthis(build,object):
         from distutils.sysconfig import customize_compiler
         compiler = new_compiler()
         customize_compiler(compiler)
-        lib_file = compiler.find_library_file([os.path.join(self.lib_prefix,'lib64')],'adolc')
+        lib_file = compiler.find_library_file([self.lib_libdir, os.path.join(self.lib_prefix,'lib64'), os.path.join(self.lib_prefix,'lib')],'adolc')
         return lib_file is None
 
     def initialize_options(self):
         super(buildthis,self).initialize_options()
         self.src = None
         self.lib_prefix = None
+        self.lib_libdir = None
         self.colpack_dir = None
         self.boost_dir = None
         self.only_swig = None
@@ -146,6 +151,13 @@ class buildthis(build,object):
             self.src = '.'
         if self.lib_prefix is None:
             self.lib_prefix = os.path.join(os.environ['HOME'],'adolc_base')
+        if self.lib_libdir is None:
+            import ctypes as c
+            plen = c.sizeof(c.c_void_p(0))
+            if plen == 8:
+                self.lib_libdir = os.path.join(self.lib_prefix,'lib64')
+            else:
+                self.lib_libdir = os.path.join(self.lib_prefix,'lib')
         if self.colpack_dir is None:
             self.colpack_dir = os.path.join(os.environ['HOME'],'adolc_base')
         if self.boost_dir is None:
@@ -163,12 +175,14 @@ class installthis(install,object):
     command_name = 'install'
     user_options = install.user_options + [
         ('lib-prefix=', None, 'prefix to install adolc library'),
+        ('lib-libdir=', None, 'libdir to install adolc library (usually inside prefix)'),
         ('colpack-dir=', None, 'directory in which colpack is installed'),
         ('boost-dir=', None, 'directory in which boost is installed'),
         ('only-swig', None, 'only install this swig module, not adolc library')]
 
     def initialize_options(self):
         self.lib_prefix = None
+        self.lib_libdir = None
         self.colpack_dir = None
         self.boost_dir = None
         self.only_swig = None
@@ -177,6 +191,7 @@ class installthis(install,object):
     def finalize_options(self):
         self.set_undefined_options('build',
                                    ('lib_prefix','lib_prefix'),
+                                   ('lib_libdir', 'lib_libdir'),
                                    ('colpack_dir', 'colpack_dir'),
                                    ('boost_dir', 'boost_dir'),
                                    ('only_swig', 'only_swig'))
