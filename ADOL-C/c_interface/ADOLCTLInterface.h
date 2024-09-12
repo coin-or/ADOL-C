ab4911370e3a00f5f560f7a95f8f224219846bc3
Tim Siebert <tim.siebert96@outlook.com>
2024-09-12 21:29:57 +0200

    add interface src

---
+++ ADOL-C/c_interface/ADOLCTLInterface.h
@@ -0,0 +1,125 @@
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    // Used to handle Tape-based adouble as void*
+    typedef void *TLAdoubleHandle;
+#ifdef __cplusplus
+}
+#endif
+
+/*
+Constructor & Destructor for class tape-less adouble
+*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    // Constructor
+    TLAdoubleHandle create_tl_adouble(double x);
+    TLAdoubleHandle create_tl_adouble_with_ad(const double val, const double *ad_val);
+    TLAdoubleHandle create_tl_adouble_empty();
+
+    // Destructor
+    void free_tl_adouble(TLAdoubleHandle a);
+#ifdef __cplusplus
+}
+#endif
+
+/*
+Utilities for adouble
+*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    void set_num_dir(const size_t n);
+    void set_tl_value(TLAdoubleHandle a, const double val);
+    void set_tl_ad_value(TLAdoubleHandle a, const double *const val);
+    void set_tl_ad_value_idx(TLAdoubleHandle a, const size_t pos, const double val);
+    double get_tl_value(TLAdoubleHandle a);
+    const double *get_tl_ad_values(TLAdoubleHandle a);
+    double get_tl_ad_value_idx(TLAdoubleHandle a, const size_t pos);
+#ifdef __cplusplus
+}
+#endif
+
+/*
+Arithmetics for class tl adouble
+*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    TLAdoubleHandle add_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle add_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle add_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle mult_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle mult_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle mult_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle subtr_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle subtr_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle subtr_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle div_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle div_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle div_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle max_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle max_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle max_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle min_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle min_double_tl_adouble(double x, TLAdoubleHandle b);
+    TLAdoubleHandle min_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle pow_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    TLAdoubleHandle pow_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    bool ge_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    bool ge_double_tl_adouble(double x, TLAdoubleHandle b);
+    bool ge_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    bool g_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    bool g_double_tl_adouble(double x, TLAdoubleHandle b);
+    bool g_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    bool le_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    bool le_double_tl_adouble(double x, TLAdoubleHandle b);
+    bool le_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    bool l_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    bool l_double_tl_adouble(double x, TLAdoubleHandle b);
+    bool l_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    bool eq_tl_adouble(TLAdoubleHandle a, TLAdoubleHandle b);
+    bool eq_double_tl_adouble(double x, TLAdoubleHandle b);
+    bool eq_tl_adouble_double(TLAdoubleHandle a, double x);
+
+    TLAdoubleHandle tl_abs(TLAdoubleHandle a);
+    TLAdoubleHandle tl_sqrt(TLAdoubleHandle a);
+    TLAdoubleHandle tl_log(TLAdoubleHandle a);
+    TLAdoubleHandle tl_log10(TLAdoubleHandle a);
+    TLAdoubleHandle tl_sin(TLAdoubleHandle a);
+    TLAdoubleHandle tl_cos(TLAdoubleHandle a);
+    TLAdoubleHandle tl_tan(TLAdoubleHandle a);
+    TLAdoubleHandle tl_exp(TLAdoubleHandle a);
+    TLAdoubleHandle tl_asin(TLAdoubleHandle a);
+    TLAdoubleHandle tl_acos(TLAdoubleHandle a);
+    TLAdoubleHandle tl_atan(TLAdoubleHandle a);
+    TLAdoubleHandle tl_sinh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_cosh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_tanh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_asinh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_acosh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_atanh(TLAdoubleHandle a);
+    TLAdoubleHandle tl_ceil(TLAdoubleHandle a);
+    TLAdoubleHandle tl_floor(TLAdoubleHandle a);
+    TLAdoubleHandle tl_ldexp(TLAdoubleHandle a, int n);
+    TLAdoubleHandle tl_erf(TLAdoubleHandle a);
+#ifdef __cplusplus
+}
+#endif
---
+++ ADOL-C/c_interface/array_handler.h
@@ -0,0 +1,17 @@
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    const double getindex_vec(const double *A, const int row);
+    void setindex_vec(double *A, const double val, const int row);
+    const double getindex_mat(const double **A, const int row, const int col);
+    void setindex_mat(double **A, const double val, const int row, const int col);
+    const double **getindex_ten(const double ***A, const int dim);
+    const double getindex_tens(const double ***A, const int dim, const int row, const int col);
+    void setindex_tens(double ***A, const double val, const int dim, const int row, const int col);
+    short **alloc_short_mat(const int row, const int col);
+    void free_short_mat(short **s);
+#ifdef __cplusplus
+}
+#endif
