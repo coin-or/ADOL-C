03ea6c6ec439d0821325e1418ff3368d7c6835ab
Tim Siebert <tim.siebert96@outlook.com>
2024-11-08 17:24:26 +0100

    change order

--- ADOL-C/src/fo_rev.c
+++ ADOL-C/src/fo_rev.c
@@ -1483,8 +1483,8 @@ int int_reverse_safe(
             case asinh_op:                                            /* asinh_op */
             case acosh_op:                                            /* acosh_op */
             case atanh_op:                                            /* atanh_op */
-            case erfc_op:                                             /* erfc_op  */
             case erf_op:                                              /* erf_op   */
+            case erfc_op:                                             /* erfc_op  */
                 res  = get_locint_r();
                 arg2 = get_locint_r();
                 arg1 = get_locint_r();
