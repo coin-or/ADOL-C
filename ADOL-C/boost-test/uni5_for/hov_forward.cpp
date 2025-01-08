697979bbb7ff1f3593be74bf21b42a702da45058
Tim Siebert <tim.siebert96@outlook.com>
2025-01-08 15:03:01 +0100

    add tests for a^x

--- ADOL-C/boost-test/uni5_for/hov_forward.cpp
+++ ADOL-C/boost-test/uni5_for/hov_forward.cpp
@@ -2574,4 +2574,75 @@ BOOST_AUTO_TEST_CASE(Atan2Operator_HOV_Forward) {
   myfree3(X);
   myfree3(Y);
 }
+
+BOOST_AUTO_TEST_CASE(Pow_Operator_HOV_Forward_1) {
+  const int16_t tag = 0;
+  const size_t dim_out = 1;
+  const size_t dim_in = 1;
+  const size_t degree = 3;
+  const size_t num_dirs = 2;
+  std::vector<double> in{0.2};
+  std::vector<adouble> indep(dim_in);
+  std::vector<double> out(dim_out);
+
+  // x^y
+  trace_on(tag);
+  indep[0] <<= in[0];
+  adouble dep = pow(indep[0], 3.2);
+  dep >>= out[0];
+  trace_off();
+
+  double ***X = myalloc3(dim_in, num_dirs, degree);
+  double ***Y = myalloc3(dim_out, num_dirs, degree);
+
+  X[0][0][0] = 1.0;
+  X[0][0][1] = -1.0;
+  X[0][0][2] = 1.1;
+
+  X[0][1][0] = 2.0;
+  X[0][1][1] = -2.1;
+  X[0][1][2] = -2.0;
+
+  std::vector<double> test_in{0.2};
+
+  // x^y
+  double test_out = std::pow(test_in[0], 3.2);
+
+  hov_forward(tag, dim_out, dim_in, degree, num_dirs, test_in.data(), X,
+              out.data(), Y);
+
+  BOOST_TEST(out[0] == test_out, tt::tolerance(tol));
+
+  // first derivative
+  BOOST_TEST(Y[0][0][0] == std::pow(test_in[0], 2.2) * 3.2 * X[0][0][0],
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][0] == std::pow(test_in[0], 2.2) * 3.2 * X[0][1][0],
+             tt::tolerance(tol));
+
+  // second derivative
+  BOOST_TEST(Y[0][0][1] == std::pow(test_in[0], 2.2) * 3.2 * X[0][0][1] +
+                               1.0 / 2.0 * std::pow(test_in[0], 1.2) * 7.04 *
+                                   X[0][0][0] * X[0][0][0],
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][1] == std::pow(test_in[0], 2.2) * 3.2 * X[0][1][1] +
+                               1.0 / 2.0 * std::pow(test_in[0], 1.2) * 7.04 *
+                                   X[0][1][0] * X[0][1][0],
+             tt::tolerance(tol));
+
+  // third derivative
+  BOOST_TEST(Y[0][0][2] == std::pow(test_in[0], 2.2) * 3.2 * X[0][0][2] +
+                               std::pow(test_in[0], 1.2) * 7.04 * X[0][0][0] *
+                                   X[0][0][1] +
+                               1.0 / 6.0 * std::pow(test_in[0], 0.2) * 8.448 *
+                                   X[0][0][0] * X[0][0][0] * X[0][0][0],
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][2] == std::pow(test_in[0], 2.2) * 3.2 * X[0][1][2] +
+                               std::pow(test_in[0], 1.2) * 7.04 * X[0][1][0] *
+                                   X[0][1][1] +
+                               1.0 / 6.0 * std::pow(test_in[0], 0.2) * 8.448 *
+                                   X[0][1][0] * X[0][1][0] * X[0][1][0],
+             tt::tolerance(tol));
+  myfree3(X);
+  myfree3(Y);
+}
 BOOST_AUTO_TEST_SUITE_END()
