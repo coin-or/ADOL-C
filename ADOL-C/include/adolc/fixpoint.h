a68ff15ed565971d3b225507570ab38cf3315c6e
Tim Siebert <tim.siebert96@outlook.com>
2025-01-21 19:37:22 +0100

    add naming for the function typdefs

--- ADOL-C/include/adolc/fixpoint.h
+++ ADOL-C/include/adolc/fixpoint.h
@@ -21,11 +21,11 @@
 BEGIN_C_DECLS
 typedef int (*double_F)(double *, double *, double *, int, int);
 typedef int (*adouble_F)(adouble *, adouble *, adouble *, int, int);
-typedef double (*norm)(double *, int);
-typedef double (*norm_deriv)(double *, int);
+typedef double (*norm_F)(double *, int);
+typedef double (*norm_deriv_F)(double *, int);
 
 ADOLC_DLL_EXPORT
-int fp_iteration(size_t sub_tape_num, double_F, adouble_F, norm, norm_deriv,
+int fp_iteration(size_t sub_tape_num, double_F, adouble_F, norm_F, norm_deriv_F,
                  double epsilon, double epsilon_deriv, size_t N_max,
                  size_t N_max_deriv, adouble *x_0, adouble *u, adouble *x_fix,
                  size_t dim_x, size_t dim_u);
