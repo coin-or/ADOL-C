c76b0617bc7f286129d27492f024996b33c71fd1
Tim Siebert <tim.siebert96@outlook.com>
2025-01-28 19:13:51 +0100

    add higher order tests for cbrt: thanks to gertrud

--- ADOL-C/boost-test/uni5_for/hov_forward.cpp
+++ ADOL-C/boost-test/uni5_for/hov_forward.cpp
@@ -2869,4 +2869,91 @@ BOOST_AUTO_TEST_CASE(PowOperator_HOV_Forward_3) {
   myfree3(X);
   myfree3(Y);
 }
+/*
+BOOST_AUTO_TEST_CASE(CbrtOperator_HOV_Forward) {
+  const int16_t tag = 0;
+  const size_t dim_out = 1;
+  const size_t dim_in = 1;
+  const size_t degree = 3;
+  const size_t num_dirs = 2;
+  std::vector<double> in{0.5};
+  std::vector<adouble> indep(dim_in);
+  std::vector<double> out(dim_out);
+
+  // cbrt(x)
+  trace_on(tag);
+  indep[0] <<= in[0];
+  adouble dep = cbrt(indep[0]);
+  dep >>= out[0];
+  trace_off();
+
+  double ***X = myalloc3(dim_in, num_dirs, degree);
+  double ***Y = myalloc3(dim_out, num_dirs, degree);
+
+  X[0][0][0] = 1.0;
+  X[0][0][1] = -1.0;
+  X[0][0][2] = 1.1;
+
+  X[0][1][0] = 2.0;
+  X[0][1][1] = -2.1;
+  X[0][1][2] = -2.0;
+
+  std::vector<double> test_in{0.5};
+
+  // cbrt(x)
+  double test_out = std::cbrt(test_in[0]);
+
+  hov_forward(tag, dim_out, dim_in, degree, num_dirs, test_in.data(), X,
+              out.data(), Y);
+
+  BOOST_TEST(out[0] == test_out, tt::tolerance(tol));
+
+  // first derivative
+  BOOST_TEST(Y[0][0][0] ==
+                 1.0 / (3.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                     X[0][0][0],
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][0] ==
+                 1.0 / (3.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                     X[0][1][0],
+             tt::tolerance(tol));
+
+  // second derivative
+  BOOST_TEST(Y[0][0][1] ==
+                 1.0 / (3.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                         X[0][0][1] +
+                     1.0 / 2.0 *
+                         (-2.0 / (9.0 * std::pow(test_in[0], 5.0 / 3.0)) *
+                          X[0][0][0] * X[0][0][0]),
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][1] ==
+                 1.0 / (3.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                         X[0][1][1] +
+                     1.0 / 2.0 *
+                         (-2.0 / (9.0 * std::pow(test_in[0], 5.0 / 3.0)) *
+                          X[0][1][0] * X[0][1][0]),
+             tt::tolerance(tol));
+
+  // third derivative
+  BOOST_TEST(Y[0][0][2] ==
+                 1.0 / (3.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                         X[0][0][2] +
+                     (-2.0 / (9.0 * std::pow(test_in[0], 5.0 / 3.0)) *
+                      X[0][0][0] * X[0][0][1]) +
+                     1.0 / 6.0 *
+                         (10.0 / (27.0 * std::pow(test_in[0], 8.0 / 3.0)) *
+                          std::pow(X[0][0][0], 3)),
+             tt::tolerance(tol));
+  BOOST_TEST(Y[0][1][2] ==
+                 1.0 / (2.0 * std::cbrt(test_in[0]) * std::cbrt(test_in[0])) *
+                         X[0][1][2] +
+                     (-2.0 / (9.0 * std::pow(test_in[0], 5.0 / 3.0)) *
+                      X[0][1][0] * X[0][1][1]) +
+                     1.0 / 6.0 *
+                         (10.0 / (27.0 * std::pow(test_in[0], 8.0 / 3.0)) *
+                          std::pow(X[0][1][0], 3)),
+             tt::tolerance(tol));
+  myfree3(X);
+  myfree3(Y);
+}*/
 BOOST_AUTO_TEST_SUITE_END()
