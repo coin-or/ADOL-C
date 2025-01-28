411e88adf59cad35618ff1c8f8a27c668686eb36
Tim Siebert <tim.siebert96@outlook.com>
2025-01-28 20:32:01 +0100

    add tests for exp fos and zos for pdouble

--- ADOL-C/boost-test/pdouble.cpp
+++ ADOL-C/boost-test/pdouble.cpp
@@ -9,40 +9,79 @@ File for explicit testing functions from uni5_for.cpp file.
 namespace tt = boost::test_tools;
 
 #include <adolc/adolc.h>
+#include <adolc/taping.h>
 
 #include "const.h"
 
 BOOST_AUTO_TEST_CASE(ExpOperator_ZOS_Forward) {
-  double a = 2., aout;
-  double p = 2.0;
-  adouble ad;
+  const int16_t tag = 0;
+  const size_t dim_out = 1;
+  const size_t dim_in = 1;
+  std::vector<double> ad_in{0.5};
+  std::vector<double> pd_in{0.5};
+  std::vector<adouble> indep(dim_in);
+  std::vector<double> out(dim_out);
 
+  // a * exp(p)
   trace_on(1);
-  ad <<= a;
+  indep[0] <<= ad_in[0];
 
-  pdouble pd(2.0);
-  std::cout << pd.loc() << std::endl;
-  ad = ad * exp(pd);
+  pdouble pd(pd_in[0]);
+  adouble dep = indep[0] * exp(pd);
 
-  ad >>= aout;
+  dep >>= out[0];
   trace_off();
 
-  a = a * std::exp(p);
+  BOOST_TEST(out[0] == ad_in[0] * std::exp(pd_in[0]), tt::tolerance(tol));
 
-  BOOST_TEST(aout == a, tt::tolerance(tol));
+  zos_forward(1, 1, 1, 0, ad_in.data(), out.data());
+  BOOST_TEST(out[0] == ad_in[0] * std::exp(pd_in[0]), tt::tolerance(tol));
 
-  double *x = myalloc1(1);
-  double *y = myalloc1(1);
+  pd_in[0] = 1.2;
 
-  *x = 2.;
+  // update pd on tape
+  set_param_vec(1, 1, pd_in.data());
 
-  zos_forward(1, 1, 1, 0, x, y);
-
-  BOOST_TEST(*y == a, tt::tolerance(tol));
-
-  myfree1(x);
-  myfree1(y);
+  zos_forward(1, 1, 1, 0, ad_in.data(), out.data());
+  BOOST_TEST(out[0] == ad_in[0] * std::exp(pd_in[0]), tt::tolerance(tol));
 }
 
+BOOST_AUTO_TEST_CASE(ExpOperator_FOS_Forward) {
+  const int16_t tag = 0;
+  const size_t dim_out = 1;
+  const size_t dim_in = 1;
+  std::vector<double> ad_in{0.5};
+  std::vector<double> pd_in{0.5};
+  std::vector<adouble> indep(dim_in);
+  std::vector<double> out(dim_out);
+
+  // a * exp(p)
+  trace_on(1);
+  indep[0] <<= ad_in[0];
+
+  pdouble pd = pdouble::mkparam(pd_in[0]);
+  adouble dep = indep[0] * exp(pd);
+
+  dep >>= out[0];
+  trace_off();
+
+  std::vector<double> X{1.1};
+  std::vector<double> Y(dim_out);
+
+  fos_forward(1, 1, 1, 0, ad_in.data(), X.data(), out.data(), Y.data());
+
+  BOOST_TEST(out[0] == ad_in[0] * std::exp(pd_in[0]), tt::tolerance(tol));
+  BOOST_TEST(Y[0] == std::exp(pd_in[0]) * X[0], tt::tolerance(tol));
+
+  pd_in[0] = 1.2;
+
+  // update pd on tape
+  set_param_vec(1, 1, pd_in.data());
+
+  fos_forward(1, 1, 1, 0, ad_in.data(), X.data(), out.data(), Y.data());
+
+  BOOST_TEST(out[0] == ad_in[0] * std::exp(pd_in[0]), tt::tolerance(tol));
+  BOOST_TEST(Y[0] == std::exp(pd_in[0]) * X[0], tt::tolerance(tol));
+}
 BOOST_AUTO_TEST_SUITE(test_pdouble)
 BOOST_AUTO_TEST_SUITE_END()
