f8e8337d18ac0a414ce1ef28b7f4072f080819a0
Stephen J. Maher <smaher@gams.com>
2025-02-18 11:38:22 +0000

    adds test script for hess_pat

---
+++ ADOL-C/swig/examples/test_hess_pat.py
@@ -0,0 +1,43 @@
+import adolc
+
+def tapeFunction(x, rowno):
+   adolc.trace_on(rowno)
+   ax = adolc.as_adouble(x)
+
+   # marking the x variables as independent
+   for item in iter(ax):
+      item.declareIndependent()
+
+   L   = ax[0]
+   Inp = ax[1]
+   Out = ax[2]
+   P   = ax[3]
+
+   ay = adolc.as_adouble(0)
+   if rowno == 0:
+      ay = ax[3] * ax[2];
+   elif rowno == 1:
+      hold1 = (pow(ax[0],(-1.0)) + pow(ax[1],(-1.0)))
+      hold2 = pow(hold1,( -1./self.Rho ))
+
+      ay = hold2
+
+   ay.declareDependent()
+   adolc.trace_off()
+
+
+def initialiseAutoDiff():
+   x = [0.5, 0.5, 0.0, 0.0]
+   tapeFunction(x, 0)
+   tapeFunction(x, 1)
+
+
+def computeHessianStructure():
+   x = [0.5, 0.5, 0.0, 0.0]
+   hesspat = adolc.hess_pat(0, x, 0)
+   assert(np.array_equal(hesspat[0], [0, 0, 0, 1]))
+   assert(np.array_equal(hesspat[1], [3, 2]))
+
+   hesspat = adolc.hess_pat(1, x, 0)
+   assert(np.array_equal(hesspat[0], [0, 2, 4, 4]))
+   assert(np.array_equal(hesspat[1], [0, 1, 0, 1]))
