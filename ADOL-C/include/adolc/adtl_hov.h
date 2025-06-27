e0a71a76b48b857338e02efaef02aff913f4c6db
Tim Siebert <tim.siebert96@outlook.com>
2025-06-27 12:47:30 +0200

    rm ADOLC_API from static member vars

--- ADOL-C/include/adolc/adtl_hov.h
+++ ADOL-C/include/adolc/adtl_hov.h
@@ -256,9 +256,9 @@ private:
   inline static bool _do_adval();
   inline static bool _do_hoval();
   inline static bool _do_indo();
-  ADOLC_API static size_t numDir;
-  ADOLC_API static size_t degree;
-  ADOLC_API static enum Mode forward_mode;
+  static size_t numDir;
+  static size_t degree;
+  static enum Mode forward_mode;
   ADOLC_API inline friend void setNumDir(const size_t p);
   ADOLC_API inline friend void setDegree(const size_t p);
   ADOLC_API inline friend void setMode(enum Mode newmode);
