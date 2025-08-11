9056d24f5badf900fde4c5a12128dc27d1af2f2f
Tim Siebert <tim.siebert96@outlook.com>
2025-08-11 15:31:30 +0200

    adapt examples to new interface

--- ADOL-C/examples/additional_examples/sparse/sparse_hessian.cpp
+++ ADOL-C/examples/additional_examples/sparse/sparse_hessian.cpp
@@ -18,210 +18,6 @@
 #include <math.h>
 
 #include <adolc/adolc.h>
-#include <adolc/adolc_sparse.h>
-
-#define tag 1
-
-double feval(double *x);
-adouble feval_ad(adouble *x);
-
-void printmat(const char *kette, int n, int m, double **M);
-
-int main() {
-  int n = 6;
-  double f, x[6];
-  adouble fad, xad[6];
-
-  int i, j;
-
-  /****************************************************************************/
-  /*******                function evaluation                   ***************/
-  /****************************************************************************/
-
-  for (i = 0; i < n; i++)
-    x[i] = log(1.0 + i);
-
-  /* Tracing of function f(x) */
-
-  trace_on(tag);
-  for (i = 0; i < n; i++)
-    xad[i] <<= x[i];
-
-  fad = feval_ad(xad);
-
-  fad >>= f;
-  trace_off();
-
-  printf("\n f = %e\n\n\n", f);
-
-  /****************************************************************************/
-  /********           For comparisons: Full Hessian                    ********/
-  /****************************************************************************/
-
-  double **H;
-  H = myalloc2(n, n);
-
-  hessian(tag, n, x, H);
-
-  printmat(" H", n, n, H);
-  printf("\n");
-
-  /****************************************************************************/
-  /*******       sparse Hessians, complete driver              ***************/
-  /****************************************************************************/
-
-  /* coordinate format for Hessian */
-  unsigned int *rind = NULL;
-  unsigned int *cind = NULL;
-  double *values = NULL;
-  int nnz;
-  int options[2];
-
-  options[0] = 0; /*                               safe mode (default) */
-  options[1] = 0; /*                       indirect recovery (default) */
-
-  sparse_hess(tag, n, 0, x, &nnz, &rind, &cind, &values, options);
-
-  printf("In sparse format:\n");
-  for (i = 0; i < nnz; i++)
-    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
-
-  free(rind);
-  rind = NULL;
-  free(cind);
-  cind = NULL;
-  free(values);
-  values = NULL;
-
-  options[0] = 0; /*                               safe mode (default) */
-  options[1] = 1; /*                                   direct recovery */
-
-  sparse_hess(tag, n, 0, x, &nnz, &rind, &cind, &values, options);
-
-  printf("In sparse format:\n");
-  for (i = 0; i < nnz; i++)
-    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
-
-  free(rind);
-  rind = NULL;
-  free(cind);
-  cind = NULL;
-  free(values);
-  values = NULL;
-
-  /*--------------------------------------------------------------------------*/
-  /*  change value of x, but not the sparsity pattern                         */
-  /*--------------------------------------------------------------------------*/
-
-  for (i = 0; i < n; i++)
-    x[i] = 2.0 * i;
-
-  /*  For comparisons: Full Hessian:                                         */
-
-  hessian(tag, n, x, H);
-
-  printmat(" H", n, n, H);
-  printf("\n");
-
-  /*  repeated call of sparse_hess with same sparsity pattern => repeat = 1 */
-
-  sparse_hess(tag, n, 0, x, &nnz, &rind, &cind, &values, options);
-
-  printf("In sparse format:\n");
-  for (i = 0; i < nnz; i++)
-    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
-
-  free(rind);
-  rind = NULL;
-  free(cind);
-  cind = NULL;
-  free(values);
-  values = NULL;
-
-  /****************************************************************************/
-  /*******        sparse Hessians, separate drivers             ***************/
-  /****************************************************************************/
-
-  /*--------------------------------------------------------------------------*/
-  /*                                                 sparsity pattern Hessian */
-  /*--------------------------------------------------------------------------*/
-
-  unsigned int **HP = NULL; /* compressed block row storage */
-  int ctrl;
-
-  HP = (unsigned int **)malloc(n * sizeof(unsigned int *));
-  ctrl = 0;
-
-  hess_pat(tag, n, x, HP, ctrl);
-
-  printf("\n");
-  printf("Sparsity pattern of Hessian: \n");
-  for (i = 0; i < n; i++) {
-    printf(" %d: ", i);
-    for (j = 1; j <= (int)HP[i][0]; j++)
-      printf(" %d ", HP[i][j]);
-    printf("\n");
-  }
-  printf("\n");
-
-  /*--------------------------------------------------------------------------*/
-  /*                                                              seed matrix */
-  /*--------------------------------------------------------------------------*/
-
-  double **Seed;
-  int p;
-  int option = 1;
-
-  /* option = 0  indirect recovery (default),
-     option = 1  direct recovery                       */
-
-  generate_seed_hess(n, HP, &Seed, &p, option);
-
-  printmat(" Seed matrix", n, p, Seed);
-  printf("\n");
-
-  /*--------------------------------------------------------------------------*/
-  /*                                                       compressed Hessian */
-  /*--------------------------------------------------------------------------*/
-
-  double **Hcomp;
-  Hcomp = myalloc2(n, p);
-
-  hess_mat(tag, n, p, x, Seed, Hcomp);
-
-  printmat("compressed H:", n, p, Hcomp);
-  printf("\n");
-
-  /*--------------------------------------------------------------------------*/
-  /*  change value of x, but not the sparsity pattern                         */
-  /*--------------------------------------------------------------------------*/
-
-  for (i = 0; i < n; i++)
-    x[i] = 2.0 * i;
-
-  /*  For comparisons: Full Hessian                                           */
-
-  hessian(tag, n, x, H);
-
-  printmat(" H", n, n, H);
-  printf("\n");
-
-  hess_mat(tag, n, p, x, Seed, Hcomp);
-
-  printmat("compressed H:", n, p, Hcomp);
-  printf("\n");
-
-  for (i = 0; i < n; i++)
-    free(HP[i]);
-  free(HP);
-
-  myfree2(H);
-  myfree2(Hcomp);
-
-  for (i = 0; i < n; i++)
-    delete[] Seed[i];
-  delete[] Seed;
-}
 
 /***************************************************************************/
 
@@ -267,4 +63,206 @@ void printmat(const char *name, int m, int n, double **M) {
       printf(" %10.4f ", M[i][j]);
   }
   printf("\n");
+}
+
+int main() {
+
+  const short tapeId = 1;
+  createNewTape(tapeId);
+  constexpr size_t dim = 6;
+  double f, x[dim];
+  adouble fad, xad[dim];
+
+  int i, j;
+
+  /****************************************************************************/
+  /*******                function evaluation                   ***************/
+  /****************************************************************************/
+
+  for (i = 0; i < dim; i++)
+    x[i] = log(1.0 + i);
+
+  /* Tracing of function f(x) */
+
+  trace_on(tapeId);
+  for (i = 0; i < dim; i++)
+    xad[i] <<= x[i];
+
+  fad = feval_ad(xad);
+
+  fad >>= f;
+  trace_off();
+
+  printf("\n f = %e\n\n\n", f);
+
+  /****************************************************************************/
+  /********           For comparisons: Full Hessian                    ********/
+  /****************************************************************************/
+
+  double **H;
+  H = myalloc2(dim, dim);
+
+  hessian(tapeId, dim, x, H);
+
+  printmat(" H", dim, dim, H);
+  printf("\n");
+
+  /****************************************************************************/
+  /*******       sparse Hessians, complete driver              ***************/
+  /****************************************************************************/
+
+  /* coordinate format for Hessian */
+  unsigned int *rind = NULL;
+  unsigned int *cind = NULL;
+  double *values = NULL;
+  int nnz;
+  int options[2];
+
+  options[0] = 0; /*                               safe mode (default) */
+  options[1] = 0; /*                       indirect recovery (default) */
+
+  ADOLC::Sparse::sparse_hess(tapeId, dim, 0, x, &nnz, &rind, &cind, &values,
+                             options);
+
+  printf("In sparse format:\n");
+  for (i = 0; i < nnz; i++)
+    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
+
+  free(rind);
+  rind = NULL;
+  free(cind);
+  cind = NULL;
+  free(values);
+  values = NULL;
+
+  options[0] = 0; /*                               safe mode (default) */
+  options[1] = 1; /*                                   direct recovery */
+
+  ADOLC::Sparse::sparse_hess(tapeId, dim, 0, x, &nnz, &rind, &cind, &values,
+                             options);
+
+  printf("In sparse format:\n");
+  for (i = 0; i < nnz; i++)
+    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
+
+  free(rind);
+  rind = NULL;
+  free(cind);
+  cind = NULL;
+  free(values);
+  values = NULL;
+
+  /*--------------------------------------------------------------------------*/
+  /*  change value of x, but not the sparsity pattern                         */
+  /*--------------------------------------------------------------------------*/
+
+  for (i = 0; i < dim; i++)
+    x[i] = 2.0 * i;
+
+  /*  For comparisons: Full Hessian:                                         */
+
+  hessian(tapeId, dim, x, H);
+
+  printmat(" H", dim, dim, H);
+  printf("\n");
+
+  /*  repeated call of sparse_hess with same sparsity pattern => repeat = 1 */
+
+  ADOLC::Sparse::sparse_hess(tapeId, dim, 0, x, &nnz, &rind, &cind, &values,
+                             options);
+
+  printf("In sparse format:\n");
+  for (i = 0; i < nnz; i++)
+    printf("%2d %2d %10.6f\n\n", rind[i], cind[i], values[i]);
+
+  free(rind);
+  rind = NULL;
+  free(cind);
+  cind = NULL;
+  free(values);
+  values = NULL;
+
+  /****************************************************************************/
+  /*******        sparse Hessians, separate drivers             ***************/
+  /****************************************************************************/
+
+  /*--------------------------------------------------------------------------*/
+  /*                                                 sparsity pattern Hessian */
+  /*--------------------------------------------------------------------------*/
+
+  unsigned int **HP = NULL; /* compressed block row storage */
+  int ctrl;
+
+  HP = (unsigned int **)malloc(dim * sizeof(unsigned int *));
+  ctrl = 0;
+
+  ADOLC::Sparse::hess_pat(tapeId, dim, x, HP, ctrl);
+
+  printf("\n");
+  printf("Sparsity pattern of Hessian: \n");
+  for (i = 0; i < dim; i++) {
+    printf(" %d: ", i);
+    for (j = 1; j <= (int)HP[i][0]; j++)
+      printf(" %d ", HP[i][j]);
+    printf("\n");
+  }
+  printf("\n");
+
+  /*--------------------------------------------------------------------------*/
+  /*                                                              seed matrix */
+  /*--------------------------------------------------------------------------*/
+
+  double **Seed;
+  int p;
+  int option = 1;
+
+  /* option = 0  indirect recovery (default),
+     option = 1  direct recovery                       */
+
+  ADOLC::Sparse::generate_seed_hess(dim, HP, &Seed, &p, option);
+
+  printmat(" Seed matrix", dim, p, Seed);
+  printf("\n");
+
+  /*--------------------------------------------------------------------------*/
+  /*                                                       compressed Hessian */
+  /*--------------------------------------------------------------------------*/
+
+  double **Hcomp;
+  Hcomp = myalloc2(dim, p);
+
+  hess_mat(tapeId, dim, p, x, Seed, Hcomp);
+
+  printmat("compressed H:", dim, p, Hcomp);
+  printf("\n");
+
+  /*--------------------------------------------------------------------------*/
+  /*  change value of x, but not the sparsity pattern                         */
+  /*--------------------------------------------------------------------------*/
+
+  for (i = 0; i < dim; i++)
+    x[i] = 2.0 * i;
+
+  /*  For comparisons: Full Hessian                                           */
+
+  hessian(tapeId, dim, x, H);
+
+  printmat(" H", dim, dim, H);
+  printf("\n");
+
+  hess_mat(tapeId, dim, p, x, Seed, Hcomp);
+
+  printmat("compressed H:", dim, p, Hcomp);
+  printf("\n");
+
+  for (i = 0; i < dim; i++)
+    free(HP[i]);
+  free(HP);
+
+  myfree2(H);
+  myfree2(Hcomp);
+
+  for (i = 0; i < dim; i++)
+    delete[] Seed[i];
+  delete[] Seed;
 }
